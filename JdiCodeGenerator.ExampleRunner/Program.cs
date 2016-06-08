41f424bb0be72b870c3dcf44c1e872e2a99e3cac
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2016-06-08 12:12:11 +0300

    .

--- JdiCodeGenerator.ExampleRunner/Program.cs
+++ JdiCodeGenerator.ExampleRunner/Program.cs
@@ -14,10 +14,10 @@
         {
             var list = new List<string>
             {
-                // "file:///C:/1/bootstrap.html"
-                // "http://localhost:1234/bootstrap/users",
-                // "http://localhost:1234/bootstrap/users/user",
-
+                // "file:///C:/1/bootstrap.html",
+                "http://localhost:1234/bootstrap/users",
+                "http://localhost:1234/bootstrap/users/user",
+                "http://localhost:1234/bootstrap",
                 // "http://localhost:1234/",
 
 
