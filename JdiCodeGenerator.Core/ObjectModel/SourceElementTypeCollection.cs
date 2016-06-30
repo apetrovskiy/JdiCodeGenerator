40cf2362200ccdb57114f461df6254a5aac79bc6
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2016-06-30 18:25:41 +0300

    refactoring

---
+++ JdiCodeGenerator.Core/ObjectModel/SourceElementTypeCollection.cs
@@ -0,0 +1,9 @@
+﻿namespace JdiCodeGenerator.Core.ObjectModel
+{
+    using System.Collections.Generic;
+
+    public class SourceElementTypeCollection<T>
+    {
+        public List<T> Types { get; set; }
+    }
+}
