9c363b39bdf017d6bd157307ead8f8dd8b0284df
Alexander Petrovskiy <apetrovskiy>
2016-06-30 11:30:06 +0300

    refactoring of unit tests and splitting into several assemblies

---
+++ JdiCodeGenerator.Core/ObjectModel/Abstract/ISourceElementTypeCollection.cs
@@ -0,0 +1,9 @@
+﻿namespace JdiCodeGenerator.Core.ObjectModel.Abstract
+{
+    using System.Collections.Generic;
+
+    public interface ISourceElementTypeCollection<T>
+    {
+        List<T> Types { get; set; }
+    }
+}
---
+++ JdiCodeGenerator.Web/ObjectModel/SourceElementTypeCollection.cs
@@ -0,0 +1,11 @@
+﻿namespace JdiCodeGenerator.Web.ObjectModel
+{
+    using System.Collections.Generic;
+    using Abstract;
+    using Core.ObjectModel.Abstract;
+
+    public class SourceElementTypeCollection<T> : ISourceElementTypeCollection<T>
+    {
+        public List<T> Types { get; set; }
+    }
+}
