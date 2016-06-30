053cf3e52a77a019946ef938ea18165d5b067ca5
Alexander Petrovskiy <apetrovskiy>
2016-06-30 22:50:00 +0300

    refactoring

---
+++ JdiCodeGenerator.Facade/CodeEntriesLoader.cs
@@ -0,0 +1,26 @@
+﻿namespace JdiCodeGenerator.Facade
+{
+    using System.Collections.Generic;
+    using Core.ObjectModel.Abstract;
+    using Web.ObjectModel.Abstract;
+
+    public class CodeEntriesLoader
+    {
+        public IEnumerable<ICodeEntry<HtmlElementTypes>> LoadWeb(string[] addresses)
+        {
+            return Load<HtmlElementTypes>(addresses);
+        }
+
+        //public IEnumerable<ICodeEntry<WinElementTypes>> LoadWinUi(string pathToApp)
+        //{
+        //    return Load<WinElementTypes>(pathToApp);
+        //}
+
+        IEnumerable<ICodeEntry<T>> Load<T>(string[] addresses)
+        {
+            var result = new List<ICodeEntry<T>>();
+
+            return result;
+        }
+    }
+}
