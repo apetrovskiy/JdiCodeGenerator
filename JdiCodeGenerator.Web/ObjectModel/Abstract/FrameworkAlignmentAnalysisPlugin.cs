eb956c4332b3ffc10dfdacac6e6794ef29d2158c
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2016-07-04 17:16:09 +0300

    support for rulesets and refactoring

---
+++ JdiCodeGenerator.Web/ObjectModel/Abstract/FrameworkAlignmentAnalysisPlugin.cs
@@ -0,0 +1,47 @@
+﻿namespace JdiCodeGenerator.Web.ObjectModel.Abstract
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using Core.ObjectModel.Abstract;
+    using Helpers;
+    using HtmlAgilityPack;
+
+    public abstract class FrameworkAlignmentAnalysisPlugin : IFrameworkAlingmentAnalysisPlugin<HtmlElementTypes>
+    {
+        public FrameworkAlignmentAnalysisPlugin()
+        {
+            Rules = new List<IRule<HtmlElementTypes>>();
+        }
+
+        public JdiElementTypes Analyze(HtmlNode node)
+        {
+            if (null == node)
+                return JdiElementTypes.StopProcessing;
+
+            // 20160704
+            // return GetJdiTypeOfElementByUsingRules(node);
+            var jdiType = GetJdiTypeOfElementByUsingRules(node);
+            if (JdiElementTypes.Element != jdiType)
+                node = null;
+
+            return jdiType;
+        }
+
+        public IEnumerable<IRule<HtmlElementTypes>> Rules { get; set; }
+        public IEnumerable<string> ExcludeList { get; set; }
+        public IRule<HtmlElementTypes> RuleThatWon { get; set; }
+        public int Priority { get; set; }
+
+        JdiElementTypes GetJdiTypeOfElementByUsingRules(HtmlNode node)
+        {
+            var firstRule = Rules.FirstOrDefault(rule => rule.IsMatch(node));
+
+            // experimental
+            RuleThatWon = firstRule;
+            ExtensionMethodsForNodes.AnalyzerThatWon = this;
+
+            // return firstRule?.TargetType ?? JdiElementTypes.Element;
+            return null == firstRule ? JdiElementTypes.Element : firstRule.TargetType;
+        }
+    }
+}
---
+++ JdiCodeGenerator.Web/ObjectModel/Plugins/PlainHtml5.cs
@@ -0,0 +1,43 @@
+﻿namespace JdiCodeGenerator.Web.ObjectModel.Plugins
+{
+    using System.Collections.Generic;
+    using Abstract;
+    using Core.ObjectModel;
+    using Core.ObjectModel.Abstract;
+
+    public class PlainHtml5 : FrameworkAlignmentAnalysisPlugin
+    {
+        public PlainHtml5()
+        {
+            Priority = 0;
+
+            Rules = new List<IRule<HtmlElementTypes>>
+            {
+                new Rule<HtmlElementTypes>
+                {
+                    Description = "Text field",
+                    SourceTypes = new List<SourceElementTypeCollection<HtmlElementTypes>> { new SourceElementTypeCollection<HtmlElementTypes> { Types = new List<HtmlElementTypes> { HtmlElementTypes.Input } } },
+                    TargetType = JdiElementTypes.TextField,
+                    OrConditions = new List<IRuleCondition>
+                    { new RuleCondition { Relationship = NodeRelationships.Self, Marker = Markers.Type, MarkerValues = new List<string> { "text", "email" } } }
+                },
+                //new Rule<HtmlElementTypes>
+                //{
+                //    Description = "Button",
+                //    SourceTypes = new List<SourceElementTypeCollection<HtmlElementTypes>> { new SourceElementTypeCollection<HtmlElementTypes> { Types = new List<HtmlElementTypes> { HtmlElementTypes.Button, HtmlElementTypes.A, HtmlElementTypes.Input } } },
+                //    TargetType = JdiElementTypes.Button,
+                //    OrConditions = new List<IRuleCondition>
+                //    { new RuleCondition { Relationship = NodeRelationships.Self, Marker = Markers.Tag, MarkerValues = new List<string> { "button" } } }
+                //}
+                new Rule<HtmlElementTypes>
+                {
+                    Description = "something non-useful",
+                    SourceTypes = new List<SourceElementTypeCollection<HtmlElementTypes>> { new SourceElementTypeCollection<HtmlElementTypes> { Types = new List<HtmlElementTypes> { HtmlElementTypes.Body } } },
+                    TargetType = JdiElementTypes.Button,
+                    OrConditions = new List<IRuleCondition>
+                    { new RuleCondition { Relationship = NodeRelationships.Self, Marker = Markers.Tag, MarkerValues = new List<string> { "hzhz" } } }
+                }
+            };
+        }
+    }
+}
