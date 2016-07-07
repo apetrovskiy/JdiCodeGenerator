ae1021f40c3c2c9ad5796416e028d9731a0900f4
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2016-07-07 22:43:05 +0300

    partial support for jQuery's Bootstrap-select; the Metals&Colors page

---
+++ JdiCodeGenerator.Web/ObjectModel/Plugins/JqueryBootstrapSelect.cs
@@ -0,0 +1,35 @@
+﻿namespace JdiCodeGenerator.Web.ObjectModel.Plugins
+{
+    using System.Collections.Generic;
+    using Abstract;
+    using Core;
+    using Core.ObjectModel;
+    using Core.ObjectModel.Abstract;
+
+    public class JqueryBootstrapSelect : FrameworkAlignmentAnalysisPlugin
+    {
+        public JqueryBootstrapSelect()
+        {
+            Priority = 50;
+
+            // https://silviomoreto.github.io/bootstrap-select/
+            // https://silviomoreto.github.io/bootstrap-select/examples/
+            Rules = new List<IRule<HtmlElementTypes>>
+            {
+                new Rule<HtmlElementTypes>
+                {
+                    Description = "ComboBox",
+                    SourceTypes = new List<SourceElementTypeCollection<HtmlElementTypes>> { new SourceElementTypeCollection<HtmlElementTypes> { Types = new List<HtmlElementTypes> { HtmlElementTypes.Div } } },
+                    TargetType = JdiElementTypes.ComboBox,
+                    AndConditions = new List<IRuleCondition> { new RuleCondition { Relationship = NodeRelationships.Self, Marker = Markers.Class, MarkerValues = new List<string> { "bootstrap-select" } } },
+                    InternalElements = new Dictionary<string, IRule<HtmlElementTypes>>
+                    {
+                        { Resources.Jdi_DropDown_root, new Rule<HtmlElementTypes> { SourceTypes = new List<SourceElementTypeCollection<HtmlElementTypes>> { new SourceElementTypeCollection<HtmlElementTypes> { Types = new List<HtmlElementTypes> { HtmlElementTypes.Div } } }, TargetType = JdiElementTypes.DropDown, OrConditions = new List<IRuleCondition> { new RuleCondition { Relationship = NodeRelationships.Self, Marker = Markers.Class, MarkerValues = new List<string> { "bootstrap-select" } } } } },
+                        { Resources.Jdi_DropDown_value, new Rule<HtmlElementTypes> { SourceTypes = new List<SourceElementTypeCollection<HtmlElementTypes>> { new SourceElementTypeCollection<HtmlElementTypes> { Types = new List<HtmlElementTypes> { HtmlElementTypes.Button } } }, TargetType = JdiElementTypes.Button, OrConditions = new List<IRuleCondition> { new RuleCondition { Relationship = NodeRelationships.Self, Marker = Markers.Class, MarkerValues = new List<string> { "dropdown-toggle" } } } } },
+                        { Resources.Jdi_DropDown_list, new Rule<HtmlElementTypes> { SourceTypes = new List<SourceElementTypeCollection<HtmlElementTypes>> { new SourceElementTypeCollection<HtmlElementTypes> { Types = new List<HtmlElementTypes> { HtmlElementTypes.Ul } } }, TargetType = JdiElementTypes.ListItem, OrConditions = new List<IRuleCondition> { new RuleCondition { Relationship = NodeRelationships.Self, Marker = Markers.Class, MarkerValues = new List<string> { "dropdown-menu" } } } } }
+                    }
+                }
+            };
+        }
+    }
+}
---
+++ Tests/JdiCodeGenerator.Tests/Recognition/JqueryBootstrapSelectTests.cs
@@ -0,0 +1,68 @@
+﻿namespace JdiCodeGenerator.Tests.Recognition
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using Core.Helpers;
+    using Core.ObjectModel;
+    using Core.ObjectModel.Abstract;
+    using HtmlAgilityPack;
+    using Internals;
+    using Web.Helpers;
+    using Web.ObjectModel.Abstract;
+    using Web.ObjectModel.Plugins;
+    using Web.ObjectModel.Plugins.BootstrapAndCompetitors;
+    using Xunit;
+
+    public class JqueryBootstrapSelectTests
+    {
+        CodeEntry<HtmlElementTypes> _entry;
+        HtmlDocument _doc;
+        readonly List<ICodeEntry<HtmlElementTypes>> _entries;
+
+        public JqueryBootstrapSelectTests()
+        {
+            _entry = null;
+            _doc = null;
+            _entries = new List<ICodeEntry<HtmlElementTypes>>();
+        }
+
+        [Theory]
+        [InlineData(@"..\Data\JqueryBootstrapSelect\Select.txt", "IComboBox<", "bootstrap-select", "dropdown-toggle", "dropdown-menu", 0)]
+        [Trait("Category", "jQuery Bootstrap-select, collection")]
+        public void ParseBootstrap3ForCollection(string input, string expected, string rootSearchString, string valueSearchString, string listSearchString, int elementPosition)
+        {
+            GivenHtmlFromFile(input);
+            WhenParsing(elementPosition);
+            ThenThereIsCollectionOfElementsOfType(expected, rootSearchString, valueSearchString, listSearchString);
+        }
+
+        void GivenHtmlFromFile(string path)
+        {
+            _doc = new HtmlDocument();
+            _doc.LoadHtml(TestFactory.GetBootstrap3Page(path));
+        }
+
+        void WhenParsing(int elementPosition)
+        {
+            var pageLoader = new PageLoader();
+            var applicableAnalyzers = new[] { typeof(JqueryBootstrapSelect) };
+            _entries.AddRange(pageLoader.GetCodeEntriesFromNode<HtmlElementTypes>(_doc.DocumentNode, TestFactory.ExcludeList, applicableAnalyzers));
+            _entry = _entries.Cast<CodeEntry<HtmlElementTypes>>().ToArray()[elementPosition];
+        }
+
+        void ThenThereIsCollectionOfElementsOfType(string expected, string rootSearchString, string valueSearchString, string listSearchString)
+        {
+            var generatedCodeEntry = _entry.GenerateCodeForEntry(SupportedLanguages.Java);
+            Assert.True(generatedCodeEntry.Contains(expected));
+            if (_entry.JdiMemberType.IsComplexControl())
+            {
+                if (!string.IsNullOrEmpty(rootSearchString))
+                    Assert.False(string.IsNullOrEmpty(_entry.Root.SearchString));
+                if (!string.IsNullOrEmpty(valueSearchString))
+                    Assert.False(string.IsNullOrEmpty(_entry.Value.SearchString));
+                if (!string.IsNullOrEmpty(listSearchString))
+                    Assert.False(string.IsNullOrEmpty(_entry.List.SearchString));
+            }
+        }
+    }
+}
